8f7a9b86f5664ba9440268572460ce0d58488739
oho-s <oho-s@klab.com>
2013-04-10 02:30:33 +0900

    firmware start writing

---
+++ devicefirm/devicefirm.ino
@@ -0,0 +1,29 @@
+/*
+ * Firmware for device
+ *
+ * Author Suguru Oho
+ * Since 2013/04/10
+ * 
+ * This file is entry point / main program of firmware for my HMD device.
+ * 
+ *
+ */
+
+// Include Space
+// For OLED drive
+#include <SPI.h>
+// For Bluetooth
+#include <SoftwareSerial.h>
+
+// Pin Define
+// For OLED drive
+#define PINRES 7
+#define PINCS 8
+#define PINDC 9
+// For Softpot (Analog)
+#define PINSPOT 3
+// For Bluetooth
+#define PINRxD 2
+#define PINTxD 3
+
+
