c2ca4a2e69ba2adb49cdd72ede52caae946f26e9
oho-s <oho-s@klab.com>
2013-04-15 02:43:02 +0900

    DEMO CODE

--- devicefirm/devicefirm.ino
+++ devicefirm/devicefirm.ino
@@ -29,6 +29,8 @@
 
 OLEDLib OLED = OLEDLib(PINRES, PINCS, PINDC);
 unsigned int color = 0;
+unsigned int count = 0;
+unsigned int state = 0;
 
 void setup() {
   OLED.init();
@@ -36,10 +38,67 @@ void setup() {
 }
 
 void loop() {
-  int i;
-  for(int i = 0;i<10;i++){
-    OLED.drawNumber(10+i*4,10,i,0xffff,0x000f);
+  switch(state){
+    case 0:
+      DEMOFill();
+      break;
+    case 1:
+      DEMOPixel();
+      break;
+    case 2:
+      DEMOLine();
+      break;
+    case 3:
+      DEMOCircle();
+      break;
+    case 4:
+      DEMONumber();
+      break;
+  }
+  count++;
+  if(count == 100){
+    state = (state+1) % 5;
+    count = 0;
+    OLED.fillRect(0, 0, 95, 63, 0x0000);
+  }
+}
+
+void DEMOFill(){
+  int x0 = random(0,95);
+  int y0 = random(0,63);
+  int x1 = random(0,95);
+  int y1 = random(0,63);
+  OLED.fillRect(x0,y0,x1,y1,color);
+  color+=16;
+}
+void DEMOPixel(){
+  int x0 = random(0,95);
+  int y0 = random(0,63);
+  OLED.putPixel(x0,y0,color);
+  color+=16;
+}
+void DEMOLine(){
+  int x0 = random(0,95);
+  int y0 = random(0,63);
+  int x1 = random(0,95);
+  int y1 = random(0,63);
+  OLED.line(x0,y0,x1,y1,color);
+  color+=16;
+}
+void DEMOCircle(){
+  int x0 = random(0,95);
+  int y0 = random(0,63);
+  int rad = random(0,30);
+  OLED.circle(x0,y0,rad,color);
+  color+=16;
+}
+
+void DEMONumber(){
+  int i,j;
+  for(i = 0;i<24;i++){
+    for(j = 0;j<10;j++){
+      OLED.drawNumber(i*4,j*6+2,(j*10+i)%10,color,0x000f);
+    }
   }
   color += 16;
 }
-
